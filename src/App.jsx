3ed9d123163790d63f8d81b5dc4cc307b7972ddc
Divay Kansal <divaykansal@Divays-MacBook-Air.local>
2024-05-02 16:48:35 +0530

    files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import Home from './components/Home';
 import Blogs from './components/Blogs';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router,  Routes, Route } from "react-router-dom";
-import  NavBar  from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-    <Router>
-        <NavBar/>
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-                <Route path="/blogs" element={<Blogs />} />            
-            </Routes>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Routes>
       </Router>
     </div>
   )
